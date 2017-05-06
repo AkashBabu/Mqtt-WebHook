77a5c15bd66675a84734ba49b14d36c2999203e7
AkashBabu <001akashbabu@gmail.com>
2017-05-06 23:04:42 +0530

    Disabled User info update

--- router/routes/mqttHooks/update.js
+++ router/routes/mqttHooks/update.js
@@ -2,6 +2,7 @@ var mqttHookColl = mongoConfig.mqttHooks.collName
 
 module.exports = function(req, res ) {
     var body = req.body || {}
+    delete body.user;
 
     helperMongo.update(mqttHookColl, body, helper.handleResult.bind(res))
 
