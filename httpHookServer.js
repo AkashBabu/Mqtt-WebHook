8d007bac04dad099f4e44402dfc1bf79d63c7225
AkashBabu <001akashbabu@gmail.com>
2017-05-06 23:04:09 +0530

    Added Mqtt Client for Mqtt Hooks

--- httpHookServer.js
+++ httpHookServer.js
@@ -1,76 +1,138 @@
+require("./config/init.globals")
+
+var mqtt = require('mqtt')
 var express = require('express')
 var bodyParser = require('body-parser')
-var jsonfile = require('jsonfile')
-var configFile = __dirname + "/config/config.json"
-var config = jsonfile.readFileSync(configFile);
 
-var mongojs = require('mongojs')
-var db = mongojs(config.db.mongo.url)
+var userColl = mongoConfig.users.collName
+var mqttHookColl = mongoConfig.mqttHooks.collName
+var mqttHookInterval = config.server.httpHook.mqttHookInterval;
+var port = config.server.httpHook.port[config.env]
 
 var app = express()
-var secretKey = "asdfASDF@1234"
 
-var jsonfile = require("jsonfile")
-var configFile = __dirname + "/config/config.json"
-
-var config = jsonfile.readFileSync(configFile)
-
-var port = config.server.httpHook.port[config.env]
 
 app.use(bodyParser.json())
 
-app.post("/:uName", function(req, res) {
-    console.log('headers:', req.headers)
-    console.log("method:", req.method)
-    console.log("user:", req.params.uName)
-    console.log("body:", req.body)
-
-    validateSecret(req, function(err) {
-        if(err) {
-             
+var mqttBuffer = [] // Buffer(Queue) to Store Mqtt Ping for 1sec
+app.post("/:uName", function (req, res) {
+    async.autoInject({
+        getUser: getUser.bind(req.params),
+        getHooks: ['getUser', getHooks.bind(req)]
+    }, function (err, results) {
+        if (err) {
+            errLog.error(err)
+            res.status(400).send("Bad Request")
         } else {
-            // var event = getEvent(req)
-
+            results.getHooks.forEach((hook) => {
+                var crypto = require('crypto')
+                var hmac = crypto.createHmac('sha1', hook.httpHook.secret)
+                var computedSignature = hmac.update(Buffer.from(JSON.stringify(req.body), 'utf-8'), 'utf-8').digest('hex')
+                if(req.headers['x-hub-signature']){
+                    if (req.headers['x-hub-signature'].split("=")[1] == computedSignature) {
+                        testLog.log('Signature Matched');
+                        mqttBuffer.push(hook)
+                    }
+                }
+            })
+            res.status(200).send("Success")
         }
     })
-
-    res.status(200).send("Success")
 })
 
-function validateSecret (req, cb) {
-    var crypto = require('crypto')
-    var hmac = crypto.createHmac('sha1', secretKey)
-    var computedKey = hmac.update(Buffer.from(JSON.stringify(req.body), 'utf-8'), 'utf-8').digest('hex')
-    console.log('Computed Secret:', computedKey);
+
+function getUser(cb) {
+    var params = this
+    db.collection(userColl).findOne({
+        uName: params.uName
+    }, {
+            _id: 1
+        }, function (err, user) {
+            if (user) {
+                cb(null, user)
+            } else {
+                if (err) {
+                    errLog.error(err);
+                }
+                cb("User Not Found")
+            }
+        })
 }
 
-function getEvent (req) {
+/**
+ * Find all the hooks associated with the User for the Event
+ */
+function getHooks(user, cb) {
+    var req = this;
+    var event = getEvent(req)
+
+    db.collection(mqttHookColl).find({ // find the matching event, that is enabled
+        user: db.ObjectId(user._id),
+        "httpHook.event": event,
+        enabled: true,
+    }, function (err, hooks) {
+        if (err) {
+            errLog.error(err)
+        }
+        cb(null, hooks || [])
+    })
+}
+/**
+ * Return the github Event from the Request Object
+ * It returns event in the form of event:reponame:ref
+ * 
+ * @returns {string}
+ */
+function getEvent(req) {
     var type = req.headers['x-github-event']
     var reponame = ""
-    if(req.body && req.body.repository && req.body.repository.name) {
+    if (req.body && req.body.repository && req.body.repository.name) {
         reponame = req.body.repository.name
     }
     var ref = ""
-    if(req.body && req.body.ref) {
+    if (req.body && req.body.ref) {
         ref = req.body.ref
     }
 
     return type + ":" + reponame + ":" + ref
 }
 
-
-app.use(function(req, res){
+// Not Found
+app.use(function (req, res) {
     res.status(404).send("Not Found")
 })
 
-app.listen(port, function(err){
-    if(err) {
-        console.error("Error Starting Http-Hook Server:", err)
+// Start Server
+app.listen(port, function (err) {
+    if (err) {
+        errLog.error("Error Starting Http-Hook Server:", err)
     } else {
-        console.log('=> HTTP-Hook Server listening on port:', port);
+        genLog.log('=> HTTP-Hook Server listening on port:', port);
     }
 })
 
-process.on("uncaughtException", function(err) {
-    console.error('Uncaught Exception', err);
+setInterval(function(){ // Mqtt Hook Publisher
+    var len = mqttBuffer.length
+    for(var i = 0; i < len; i++) {
+        var hook = mqttBuffer.shift()
+        try { // Start a new client for each ping
+            var broker = (hook.mqttHook.broker.split("://").length > 1) ? hook.mqttHook.broker.split("://")[1] : hook.mqttHook.broker.split("://")[0]  
+            testLog.log('Broker:', broker);
+            testLog.log('Hook:', hook);
+        	var client = mqtt.connec("mqtt://" + broker)
+            client.on('connect', function() {
+                testLog.log('Client Connected');
+                client.publish(hook.mqttHook.topic, JSON.stringify({
+                    event: hook.httpHook.event
+                }))
+            })
+        } catch(err) {
+            errLog.error(err);
+            errLog.error('Failed to Publish Event:', hook.httpHook.event, 'on Topic:', hook.mqttHook.topic, 'via Broker:', hook.mqttHook.broker );
+        }
+    }
+}, mqttHookInterval)
+
+process.on("uncaughtException", function (err) {
+    errLog.error('Uncaught Exception', err);
 })
