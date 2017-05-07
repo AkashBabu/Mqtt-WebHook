e72f60590e2d4b62f99731f8ba527a72af5b4980
AkashBabu <001akashbabu@gmail.com>
2017-05-07 15:47:26 +0530

    Minor bug fix

--- setup/firstTime/createAdmin.js
+++ setup/firstTime/createAdmin.js
@@ -1,25 +1,28 @@
-require("../config/init.globals")
+require("../../config/init.globals")
 
-var username = process.argv[2]
+var userName = process.argv[2]
 var password = process.argv[3]
-if (!password || !username) {
+if (!password || !userName) {
     throw new Error("Please follow Command Line Args: node createAdmin.js <userName> <password>")
 }
 
 var user = {
     name: "Admin",
     uName: userName,
-    password: helper.saltHash(password),
+    pwd: helper.saltHash(password),
     email: "admin@mqttWebHook.com",
-    role: 'ADMIN'
+    role: 'ADMIN',
+    isDeleted: false
 }
 
 var userColl = mongoConfig.users.collName
 
 db.collection(userColl).insert(user, function (err, result) {
     if (!err) {
-        console.log('Successfully Created an ADMIN user with UserName:', user.name, 'and password:', password);
+        console.log('Successfully Created an ADMIN user with UserName:', userName, 'and password:', password);
     } else {
         console.log('Mongo Err:', err);
     }
+
+    process.exit(1);
 })
